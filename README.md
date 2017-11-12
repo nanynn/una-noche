6fb89dd9068287c14b636a9426bd4f6a59c4347c
Anayn Pavez <nanyn.pavez@gmail.com>
2017-11-12 20:50:40 -0300

    version con video y scroll

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Juego de educación sexual
 Utilización de frameworks bootstrap para columnas responsivas
 
 Animación de texto de header
-créditos Ashfaq Amin y link de versión https://codepen.io/ashfaqamin/pen/dpEZJa?limit=all&page=4&q=animated+text
+Créditos Ashfaq Amin y link de versión https://codepen.io/ashfaqamin/pen/dpEZJa?limit=all&page=4&q=animated+text
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
 var button = document.querySelector('button');
 
-button.addEventListener('click', function(e){
-  e.preventDefault();
+button.addEventListener('click', function(e) {
+    e.preventDefault();
+});
+
+$(document).ready(function($) {
+    $('#primera', '#segunda', '#tercera', '#cuarta').click(function(event) {
+        $('html, body').animate({ scrollTop: 0 }, 'slow');
+        return false;
+    });
 });
