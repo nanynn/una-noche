a6ca953a8ed4faf60ff5991232927c592aac4286
Yermain Araya <Yermain Araya>
2017-11-13 16:55:24 -0300

    video y formulario

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,4 +10,31 @@ $(document).ready(function() {
         $('html, body').animate({ scrollTop: 0 }, 'slow');
         return false;
     });
+
+    $('#pago').slider({
+        tooltip: 'always'
+    });
+
+    $('#pago').on('slide', function(e){
+        $('#valor-pago').text('$' + e.value);
+    });
+
+    $('#opinion').submit(function(e){
+        e.preventDefault();
+
+        $.ajax({
+            method: 'POST',
+            url: 'https://formspree.io/chirimoyaestudio@gmail.com',
+            data: $('#opinion').serialize(),
+            datatype: 'json',
+            success: function(){
+                swal('¡Muchas Gracias!','Tu opinión ha sido enviada', 'success');
+                $('#video').modal('hide');
+
+            },
+            error: function(){
+                swal('¡Lo sentimos!','Hubo un error, inténtalo más tarde', 'error');
+            }
+        });
+    });
 });
